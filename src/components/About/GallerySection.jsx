999ffa6b6927e8f662b22f7d306259333d8d11c8
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2025-12-04 18:10:26 +0600

    gellery section images add

--- src/components/About/GallerySection.jsx
+++ src/components/About/GallerySection.jsx
@@ -14,35 +14,89 @@ const GallerySection = () => {
             />
           </div>
           {/* Image Gallery */}
-          {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:grid-rows-3 gap-4 md:h-[700px]">
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/mgH3pRB3/gallery05.jpg" alt="Event 1" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2">
-                            <Image height={266} width={266} src="https://i.postimg.cc/TYJvLyKF/gallery08.jpg" alt="Event 2" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/Tw5XpCbJ/gallery11.jpg" alt="Event 3" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/J4gcQtV4/gallery06.jpg" alt="Event 4" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/yxqC9cX4/gallery12.jpg" alt="Event 5" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/Fs1Vc36B/gallery07.jpg" alt="Event 6" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/vTWFvT31/gallery09.jpg" alt="Event 7" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/MKS8p0p1/gallery10.jpg" alt="Event 8" className="w-full h-full object-cover" />
-                        </div>
-                        <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
-                            <Image height={266} width={266} src="https://i.postimg.cc/66RXKbt9/gallery13.jpg" alt="Event 9" className="w-full h-full object-cover" />
-                        </div>
-                    </div> */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 md:grid-rows-3 gap-4 md:h-[700px]">
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/mgH3pRB3/gallery05.jpg"
+                alt="Event 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/TYJvLyKF/gallery08.jpg"
+                alt="Event 2"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/Tw5XpCbJ/gallery11.jpg"
+                alt="Event 3"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/J4gcQtV4/gallery06.jpg"
+                alt="Event 4"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/yxqC9cX4/gallery12.jpg"
+                alt="Event 5"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/Fs1Vc36B/gallery07.jpg"
+                alt="Event 6"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/vTWFvT31/gallery09.jpg"
+                alt="Event 7"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/MKS8p0p1/gallery10.jpg"
+                alt="Event 8"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="w-full h-auto p-2 col-span-2 row-span-2 md:col-span-1 md:row-span-1">
+              <Image
+                height={266}
+                width={266}
+                src="https://i.postimg.cc/66RXKbt9/gallery13.jpg"
+                alt="Event 9"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
