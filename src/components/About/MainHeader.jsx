82efe023a165d71137ad6ecda0e2936d7c9dd830
DeveloperImran1 <abdulquddus01925251187@gmail.com>
2025-12-04 18:11:55 +0600

    Main header file created

--- src/components/About/MainHeader.jsx
+++ src/components/About/MainHeader.jsx
@@ -19,9 +19,12 @@ const MainHeader = () => {
               workshops and festivals. Book your tickets easily and enjoy a
               seamless event experience.
             </p>
-            {/* <button data-aos="fade-right" className="bg-blue-500 py-2 px-6 mt-4 rounded-md hover:bg-blue-600 transition duration-300">
-                        Explore More
-                    </button> */}
+            <button
+              data-aos="fade-right"
+              className="bg-blue-500 py-2 px-6 mt-4 rounded-md hover:bg-blue-600 transition duration-300"
+            >
+              Explore More
+            </button>
           </div>
         </div>
       </div>
